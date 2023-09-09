58c2f378952755b7e33d12935c23083475914ec0
Avijit <contact.roy17@gmail.com>
2023-09-10 01:54:37 +0200

    Dashboard components created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Dashboard />
     </div>
   );
 }
---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Swal from 'sweetalert2'
+
+import employeesData from '../../data'
+
+import Add from './Add'
+import List from './List'
+import Edit from './Edit'
+import Header from './Header'
+
+function Dashboard() {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
